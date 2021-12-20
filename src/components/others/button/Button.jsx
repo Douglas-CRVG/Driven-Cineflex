4466f478f9872bb8ff83c01c77d6d10aeaa5b3f2
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2021-12-20 15:09:30 -0300

    Hotfix object sendo sucess page

--- src/components/others/button/Button.jsx
+++ src/components/others/button/Button.jsx
@@ -5,11 +5,12 @@ export default function Button({text, bool, buyer}) {
     const navigate = useNavigate();
 
     function validatePurchase(){
+        console.log(buyer)
         const {
             buySeats,
             buyerData
         } = buyer;
-        if((buySeats.length > 0) && (buyerData.name !== "") && (buyerData.cpf.length === 11)){
+        if((buySeats.length > 0) && (buyerData?.name !== "") && (buyerData?.cpf?.length === 11)){
             let body = {
                 ids: buySeats.map(seat=> seat.id),
                 name: buyerData.name,
